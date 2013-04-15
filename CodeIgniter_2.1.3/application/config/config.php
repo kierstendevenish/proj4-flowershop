e7bb1d52ad88ea186a546e6bb6ee2a670e1432a6
Kiersten <kiersten@example.com>
2013-04-15 01:30:35 -0600

    config

--- CodeIgniter_2.1.3/application/config/config.php
+++ CodeIgniter_2.1.3/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://students.cs.byu.edu/~kdevenis/CS462-4/CodeIgniter_2.1.3/';
+$config['base_url']	= 'http://students.cs.byu.edu/~kdevenis/proj4/flowershop/proj4-flowershop/CodeIgniter_2.1.3/';
 
 /*
 |--------------------------------------------------------------------------
